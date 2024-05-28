a562df3f885dc96ec28cc864dd03c4133fcc5131
Emmanuel-Adewale <adewaleemmanuel526@gmail.com>
2024-05-28 18:01:13 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,21 @@
+//A calclator program done by emmanuel Adewale 
+
+const display=document.getElementById("display")
+
+function appendToDisplay( input) {
+    display.value += input;
+}
+
+function cleardisplay(){
+    display.value="";
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="error";
+    }
+
+}
